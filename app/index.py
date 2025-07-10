1e3063746edb9f3f4bd4e5604ccc35c17c2c10c1
LDZ1704 <lamn9049@gmail.com>
2025-07-10 17:11:33 +0700

    update templates v2

--- app/index.py
+++ app/index.py
@@ -1,3 +1,4 @@
+from app import app
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return(render_template('index.html'))
+
+@app.route('/login')
+def login():
+    return(render_template('login.html'))
+
+@app.route('/register')
+def register():
+    return(render_template('register.html'))
+
+@app.route('/examdetail')
+def exam_detail():
+    return(render_template('examdetail.html'))
+
+@app.route('/account')
+def account_detail():
+    return(render_template('account.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
